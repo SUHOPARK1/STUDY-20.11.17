012e6a7bfa07ddfca74d4401a605cd76c23cef60
SUHOPARK1 <qkrtnghsla@naver.com>
2020-11-17 21:00:36 +0900

    Add Comment

--- Main.java
+++ Main.java
@@ -2,21 +2,18 @@ package self;
 
 import java.util.Scanner;
 import self.Calculator;
-
+import java.util.*;
 public class Main {
 	public static void main(String[] args) {
+		
 		Scanner scanner = new Scanner(System.in);
 		Calculator cal = new Calculator();
 		
 		
-		System.out.println("1번째 숫자");
-		int num1 = scanner.nextInt();
+		System.out.println(cal.setNum1(scanner.nextInt()));
+//		String result =  getNum1 + getNum2;
+		System.out.println("합: ");
 		
-		System.out.println("2번째 숫자");
-		int num2 = scanner.nextInt();
-		
-		System.out.println("합: ", cal(int num1+int num2));
-		System.out.println(cal.(setNum1+setNum2));
 		
 		
 		
